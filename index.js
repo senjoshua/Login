536dc81e35be43b7141264eb904e0f566733220c
senjoshua <senjoshu@gmail.com>
2019-08-05 19:53:09 -0400

    added hashing

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var port = 3000;
 var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
 
 app.use(bodyParser.json()); 
 app.use(express.static('public')); 
@@ -10,7 +11,8 @@ app.use(bodyParser.urlencoded({
 })); 
 
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/login");
+mongoose.connect("mongodb://localhost:27017/Login");
+
 var db=mongoose.connection; 
 db.on('error', console.log.bind(console, "connection error")); 
 db.once('open', function(callback){ 
@@ -21,8 +23,8 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/home", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+app.post("/logout", (req, res) => {
+    res.redirect("/");
 });
 
 var UserSchema = new mongoose.Schema({
@@ -43,30 +45,55 @@ var UserSchema = new mongoose.Schema({
     }
   });
 
-  mongoose.model('User', UserSchema);
+  var User = mongoose.model('User', UserSchema);
+  module.exports = User;
 
 app.post("/signup", (req, res) => {
-    var name = request.body.username;
-    var email = request.body.email;
-    var password = request.body.password;
-    
-    var data = { 
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    bcrypt.hash(password, 10, function(err, hash) {
+      
+      var data = new User({ 
         "name": name, 
         "email":email, 
         "password":password
-    } 
-
-    db.collection('users').insertOne(data,function(err, collection){ 
-        if (err) throw err; 
-        console.log("User added successfully");  
-    }); 
-
-    // return res.redirect("profile.html"); 
+      });
+  
+      data.save(function(error) {
+        console.log("User added successfully");
+        if (error) {
+          console.error(error);
+        }
+      });
+      
+    });
+    
+    return res.redirect("profile.html"); 
 });
 
 app.post("/signin", (req, res) => {
-    var email = request.body.email;
-    var password = request.body.password;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    db.collection('users').findOne({ 
+        'email': req.body.email}, function(err, user) {
+        // if (err) throw err; 
+        bcrypt.compare(user.password, password, function(err, isMatch) {
+          if (err) throw err; 
+          
+          return res.redirect("profile.html");
+        })
+
+          // if (user) {
+          //   return res.redirect("profile.html");
+          // } 
+          // else {
+          //   //   alert("Incorrect login info!");
+          //     return res.redirect("/");
+          // }
+       })
 
 });
 
